31b0188350b825335425aaab35d8d47dd602fdac
rootpandax <52450467+rootpandax@users.noreply.github.com>
2020-07-21 04:11:25 +0700

    Create tools.sh

---
+++ tools.sh
@@ -0,0 +1,140 @@
+#!/system/xbin/bash
+#Don't Fcking recode my code
+#jangan recode tod
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+clear
+toilet -f slant -F gay "TO-PANDA" | lolcat
+sleep 2
+printf "\033[35;1m Nickname mu siapa? : "
+read nick
+sleep 2
+echo welcome $nick lol
+sleep 2
+clear
+sleep 2
+printf "\033[35;1m Sabar Tod"
+clear
+toilet -f slant -f gay "TO-PANDA" | lolcat
+sleep 0.5
+printf "\033[31;1m By : Panda"
+printf ""
+sleep 0.5
+printf "\033[32;1m Instagram : @mfakhri_F"
+printf ""
+sleep 0.5
+printf ""
+printf "\033[36;1m jangan Recode blok"
+printf "\033[31;1m"
+echo "Tolong Gunakan Tools dengan bijak"
+echo ""
+printf "\033[34;1m"
+echo ""
+echo "=======================================================>"
+echo "[1].sqlmap
+[2].spamcaller
+[3].sublist3r
+[4].nikto
+[5].zphisher
+[6].Lacak lokasi
+[7].Virusdroidcreator
+[8].phoneinfoga
+[9].Osi.ig
+[10].aquatone
+[11].blackeye
+[12].WAScan
+[13].sqliv
+[14].XSpear
+[15].Exit
+'
+echo "=======================================================>"
+echo ""
+read -p "masukkan pilihan : " pil
+if [ $pil == 1 ]; then
+clear
+git clone https://github.com/sqlmapproject/sqlmap
+cd sqlmap
+python sqlmap.py 
+fi
+
+if [ $pil == 2 ]; then
+clear
+git clone https://github.com/rootpandax/Spamc4ll
+cd Spamc4ll
+./spamcaller.sh
+fi
+
+if [ $pil == 3 ]; then
+clear
+git clone https://github.com/aboul3la/Sublist3r
+fi
+
+if [ $pil == 4 ]; then
+clear
+git clone https://github.com/sullo/nikto
+cd nikto
+cd program
+perl nikto.pl
+fi
+
+if [ $pil == 5 ]; then
+clear
+git clone https://github.com/htr-tech/zphisher
+cd zphisher
+./zphisher.sh
+fi
+
+if [ $pil == 6 ]; then
+clear
+git clone https://github.com/thewhiteh4t/seeker
+fi
+
+if [ $pil == 7 ]; then
+clear
+git clone 
+fi
+
+if [ $pil == 8 ]; then
+clear
+git clone https://github.com/saydog/Virus-droid-creator
+fi
+
+if [ $pil == 10 ]; then
+clear
+gem install aquatone
+aquatone --help
+fi
+
+if [ $pil == 11 ]; then
+clear
+git clone https://github.com/An0nUD4Y/blackeye
+fi
+
+if [ $pil == 12 ]; then
+clear
+git clone https://github.com/m4ll0k/WAScan
+fi
+
+if [ $pil == 13 ]; then
+clear
+git clone https://github.com/the-robot/sqliv
+fi
+
+if [ $pil == 14 ]; then
+clear
+gem install XSpear
+Xspear --help
+fi
+
+if [ $pil == 15 ]; then
+clear
+echo by $nick
+exit
+fi
+;
+
