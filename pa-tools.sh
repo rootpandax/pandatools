9df366f0a76367d51efaf3cb3013accc8c77448e
rootpandax <52450467+rootpandax@users.noreply.github.com>
2020-07-21 04:30:18 +0700

    Update and rename tools.sh to pa-tools.sh
    pkg upgrade &&  update
    pkg install git
    pkg install openssh
    pkg install python
    pkg install python2
    pkg install python3

--- pa-tools.sh
+++ pa-tools.sh
@@ -1,6 +1,8 @@
 #!/system/xbin/bash
 #Don't Fcking recode my code
 #jangan recode tod
+#recode gk bakal bikin lu jadi mastah
+
 blue='\033[34;1m'
 green='\033[32;1m'
 purple='\033[35;1m'
@@ -9,7 +11,7 @@ red='\033[31;1m'
 white='\033[37;1m'
 yellow='\033[33;1m'
 clear
-toilet -f slant -F gay "TO-PANDA" | lolcat
+toilet -f slant "TO-PANDA" | lolcat
 sleep 2
 printf "\033[35;1m Nickname mu siapa? : "
 read nick
@@ -19,8 +21,9 @@ sleep 2
 clear
 sleep 2
 printf "\033[35;1m Sabar Tod"
+sleep 2
 clear
-toilet -f slant -f gay "TO-PANDA" | lolcat
+toilet -f slant "TO-PANDA" | lolcat
 sleep 0.5
 printf "\033[31;1m By : Panda"
 printf ""
@@ -30,13 +33,14 @@ printf ""
 sleep 0.5
 printf ""
 printf "\033[36;1m jangan Recode blok"
-printf "\033[31;1m"
-echo "Tolong Gunakan Tools dengan bijak"
+sleep 0.5
+printf ""
+printf "\033[35;1m Tolong Gunakan Tools dengan bijak"
 echo ""
-printf "\033[34;1m"
+printf "\033[37;1m"
 echo ""
 echo "=======================================================>"
-echo "[1].sqlmap
+echo '[1].sqlmap
 [2].spamcaller
 [3].sublist3r
 [4].nikto
@@ -52,29 +56,35 @@ echo "[1].sqlmap
 [14].XSpear
 [15].Exit
 '
-echo "=======================================================>"
 echo ""
+echo "=======================================================>"
+
 read -p "masukkan pilihan : " pil
-if [ $pil == 1 ]; then
+
+if [ $pil = 1 ]
+then
 clear
 git clone https://github.com/sqlmapproject/sqlmap
 cd sqlmap
 python sqlmap.py 
 fi
 
-if [ $pil == 2 ]; then
+if [ $pil = 2 ]
+then
 clear
 git clone https://github.com/rootpandax/Spamc4ll
 cd Spamc4ll
-./spamcaller.sh
+bash spamcall.sh
 fi
 
-if [ $pil == 3 ]; then
+if [ $pil = 3 ]
+then
 clear
 git clone https://github.com/aboul3la/Sublist3r
 fi
 
-if [ $pil == 4 ]; then
+if [ $pil = 4 ]
+then
 clear
 git clone https://github.com/sullo/nikto
 cd nikto
@@ -82,59 +92,69 @@ cd program
 perl nikto.pl
 fi
 
-if [ $pil == 5 ]; then
+if [ $pil = 5 ]
+then
 clear
 git clone https://github.com/htr-tech/zphisher
 cd zphisher
 ./zphisher.sh
 fi
 
-if [ $pil == 6 ]; then
+if [ $pil == 6 ]
+then
 clear
 git clone https://github.com/thewhiteh4t/seeker
 fi
 
-if [ $pil == 7 ]; then
+if [ $pil = 7 ]
+then
 clear
 git clone 
 fi
 
-if [ $pil == 8 ]; then
+if [ $pil = 8 ]
+then
 clear
 git clone https://github.com/saydog/Virus-droid-creator
 fi
 
-if [ $pil == 10 ]; then
+if [ $pil = 10 ]
+then
 clear
 gem install aquatone
 aquatone --help
 fi
 
-if [ $pil == 11 ]; then
+if [ $pil = 11 ]
+then
 clear
 git clone https://github.com/An0nUD4Y/blackeye
 fi
 
-if [ $pil == 12 ]; then
+if [ $pil = 12 ]
+then
 clear
 git clone https://github.com/m4ll0k/WAScan
 fi
 
-if [ $pil == 13 ]; then
+if [ $pil = 13 ]
+then
 clear
 git clone https://github.com/the-robot/sqliv
 fi
 
-if [ $pil == 14 ]; then
+if [ $pil = 14 ]
+then
 clear
 gem install XSpear
 Xspear --help
 fi
 
-if [ $pil == 15 ]; then
+if [ $pil = 15 ]
+then
 clear
 echo by $nick
 exit
 fi
-;
+
 
